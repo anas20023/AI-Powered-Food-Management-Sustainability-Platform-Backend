8769fb397a39f4e7647f5baa0277e4924de3247f
Anas Ibn Belal <anasibnebelal400@gmail.com>
2025-11-19 20:58:26 +0600

    Prisma Schema Creation and Migration

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -7,5 +7,5 @@ const redisClient = createClient({
 redisClient.on("connect", () => console.log("✅ Redis connected"));
 redisClient.on("error", (err) => console.error("❌ Redis error:", err));
 
-await redisClient.connect(); 
+// await redisClient.connect(); 
 export default redisClient;
