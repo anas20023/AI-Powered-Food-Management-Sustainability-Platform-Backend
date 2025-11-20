72b77a75207820f925f6bb35ccb58c9178079be6
Anas Ibn Belal <anasibnebelal400@gmail.com>
2025-11-20 20:07:07 +0600

    Documenting APIs

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,10 +3,7 @@ import * as authController from "../controllers/auth.controller.js";
 import auth from "../middlewares/auth.middleware.js"
 
 const router = express.Router();
-/**
-*@openapi
-* /api/auth
- */
+
 router.post("/register", authController.register); //done
 router.post("/login", authController.login); // done
 router.get("/me", auth, authController.me); // done
