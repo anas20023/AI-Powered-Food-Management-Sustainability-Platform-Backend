7414ff8aa316699641a35b069d48fac1ba7bf1d4
Anas Ibn Belal <anasibnebelal400@gmail.com>
2025-11-21 09:17:53 +0600

    Waste Gen Implemented !

--- src/services/consume.service.js
+++ src/services/consume.service.js
@@ -2,8 +2,8 @@ import prisma from '../config/PrismaClient.js';
 export const getconsumption = async (id) => {
     console.log(id);
     const res = await prisma.consumption.findMany({
-        where:{
-            user_id:id
+        where: {
+            user_id: id
         }
     });
     console.log(res);
@@ -12,12 +12,19 @@ export const getconsumption = async (id) => {
 }
 export const getwaste = async (id) => {
     // console.log(id);
-    const res = await prisma.consumption.findMany({
-        where:{
-            user_id:id
+    const res = await prisma.inventory.findMany({
+        where: {
+            user_id: id
         }
     });
     // console.log(res);
     if (!res) throw new Error("No Data Found!")
-    return res;
+    //return res;
+    const wastes = [];
+    res.forEach(element => {
+        if (element.status === "Expired") {
+            wastes.push(element)
+        }
+    });
+    return wastes
 }
